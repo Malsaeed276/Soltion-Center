91ea4efc53c25c98d7871dcaebe602836ed703ce
Mohamed Elsaeed <mohamed.elsaeed276@gmail.com>
2023-06-21 15:36:11 +0300

    Add intro Screen

---
+++ soltion_center/lib/view/login/intro.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:soltion_center/utils/images/logo.dart';
+
+import '../../controllers/localization_controller.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lang =
+        Provider.of<LocalizationController>(context, listen: true)
+            .getLanguage();
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Provider.of<LocalizationController>(context, listen: false).getLanguageDialog(context);
+            },
+            icon: Icon(
+              Icons.language_outlined,
+              color: theme.primaryColor,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomPaint(
+                size: Size(200, (200 * 1).toDouble()),
+                painter: AppLogo(),
+              ),
+              Text(
+                lang.appTitle!,
+                style: theme.textTheme.headlineMedium,
+              ),
+              Text(
+                lang.appDescription!,
+                style: theme.textTheme.bodyMedium,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      theme.colorScheme.primary,
+                    ),
+                    //overlayColor: MaterialStateProperty.all(themeColor.primary),
+                  ),
+                  child: Text(
+                    lang.accept!,
+                    style: theme.textTheme.bodyMedium?.copyWith(
+                      color: theme.colorScheme.onPrimary,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
