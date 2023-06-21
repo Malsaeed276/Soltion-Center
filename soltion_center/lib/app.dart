d19e5d3b578b979c76abcb7a79b51b5c823174d9
Mohamed Elsaeed <mohamed.elsaeed276@gmail.com>
2023-06-21 15:36:20 +0300

    update the app folder

--- soltion_center/lib/app.dart
+++ soltion_center/lib/app.dart
@@ -1,6 +1,11 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'controllers/localization_controller.dart';
+import 'over_screens/over_screens.dart';
+import 'units/theme.dart';
+import 'utils/images/logo.dart';
+import 'view/login/intro.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,27 +14,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title:  Provider.of<LocalizationController>(context, listen: false).getLanguage().appTitle!,
+      themeMode: ThemeMode.system,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: lightColorScheme,
+        brightness: Brightness.light,
       ),
-      //home:
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+        brightness: Brightness.dark,
+      ),
+        initialRoute: '/intro',
+
+      routes: {
+        '/intro': (context) => const ApplyForEachPage(
+          child: IntroPage(),
+        ),
+      },
+
     );
   }
 }
