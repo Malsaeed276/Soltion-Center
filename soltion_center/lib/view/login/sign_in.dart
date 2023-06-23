fc35e7a92c873286825fa041615ee3286def59e3
aysegulkarakose <akarakose05@gmail.com>
2023-06-24 01:15:03 +0300

    signinscreen

--- soltion_center/lib/view/login/sign_in.dart
+++ soltion_center/lib/view/login/sign_in.dart
@@ -1,12 +1,58 @@
+import 'dart:html';
+import 'package:soltion_center/units/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:soltion_center/units/logo.dart';
 
 class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
-    );
-  }
+    return Scaffold(
+      body: Column(
+        children:[
+              Container(
+                child: CustomPaint(
+                  size: Size(200, (200 * 1).toDouble()),
+                  painter: AppLogo(),
+                ),
+              ),
+              Container(
+                child: TextFormField(decoration: InputDecoration(labelText: 'e-mail', hintText: 'e-mailinizi giriniz'),
+                controller: emailController,
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'şifre', hintText: 'şifrenizi giriniz'),
+                  controller: passwordController),
+              ),
+
+              Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: lightColorScheme.surfaceTint ,
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+                onPressed:()  {
+                         
+                        },
+                child: Text(
+                  "Log In",
+                  style: TextStyle(
+                    color: lightColorScheme.onPrimary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ),
+            ),]
+              )
+      ,
+        );
+}
 }
