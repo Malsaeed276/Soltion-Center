60818e01d5f28e64de2e1cba3b55e9982bb78a41
Beyzasultanokay <136925151+Beyzasultanokay@users.noreply.github.com>
2023-07-09 14:38:16 +0300

    Update homepage.dart

--- soltion_center/lib/view/homepage/homepage.dart
+++ soltion_center/lib/view/homepage/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Anasayfa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,52 +15,56 @@ class Anasayfa extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.notifications),
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.person),
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              lang.welcomeMessage,
-              style: theme.textTheme.headline4,
-            ),
-            SizedBox(height: 16),
-            Text(
-              lang.writeIssue,
-              style: theme.textTheme.bodyText1,
-            ),
-            SizedBox(height: 24),
-            Container(
-              width: 200,
-              height: 40,
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surfaceVariant,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.search, color: theme.colorScheme.primary),
-                  SizedBox(width: 8),
-                  Text(
-                    lang.search,
-                    style: TextStyle(
-                      color: theme.colorScheme.onPrimary,
-                    ),
+      body: Container(
+        height: MediaQuery.of(context).size.height / 2,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  lang.welcomeMessage,
+                  style: theme.textTheme.headline4,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  lang.writeIssue,
+                  style: theme.textTheme.bodyText1,
+                ),
+                SizedBox(height: 24),
+                Container(
+                  width: 200,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.surfaceVariant,
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                ],
-              ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.search, color: theme.colorScheme.primary),
+                      SizedBox(width: 8),
+                      Text(
+                        lang.search,
+                        style: TextStyle(
+                          color: theme.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
